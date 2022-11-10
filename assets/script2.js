c551e6b1ab49abe0e5784e1b4ee70e54e9692f79
Riley <94302477+Rilaey@users.noreply.github.com>
2022-11-09 21:06:28 -0500

    wrote form on page 2 of hmtl and added fetch function for parks api

--- assets/script2.js
+++ assets/script2.js
@@ -1,3 +1,4 @@
+// functionallity for nav bar
 var prev = 0;
 var $window = $(window);
 var nav = $('.scrollhide-nav');
@@ -7,3 +8,16 @@ $window.on('scroll', function(){
   nav.toggleClass('hidden', scrollTop > prev);
   prev = scrollTop;
 });
+
+// fetch api request for parks api
+function callApi() {
+  fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=rvX9bJoyPEXTv5dRShhVJwdVf4vkUjRX1Odiws42')
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(data) {
+    console.log(data)
+  })
+}
+
+callApi()
